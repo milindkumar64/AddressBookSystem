5171499b198d713f01948eb17830ec53eb115b0e
milindkumar64 <milindgupta@gmail.com>
2023-04-02 14:56:07 +0530

    Add: Ability to create Contacts in AddressBooK

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class AddressBook extends Contact{
+
+	public AddressBook(String firstName, String lastName, String address, String city, String state, int zipCode,
+			long phoneNumber, String email) {
+		super(firstName, lastName, address, city, state, zipCode, phoneNumber, email);
+		
+	}
+
+	
+}
--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -3,9 +3,8 @@ package com.bridgelabz;
 public class AddressBookMain {
 
 	public static void main(String[] args) {
-	
+
 		System.out.println("Welcome to Address Book System : ");
 
 	}
-
 }
