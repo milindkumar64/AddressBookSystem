acbfd87cb06ee817a1c2125dafe314e4453c1323
milindkumar64 <milindgupta@gmail.com>
2023-04-16 20:29:06 +0530

    Add: Ability to ensure There is no duplicate Contacts

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -16,13 +16,6 @@ public class AddressBook extends Contact {
 
 	ArrayList<Contact> contacts = new ArrayList<>();
 
-	/*
-	 * public void getContactDetails() {
-	 * System.out.println("Enter details Of A Contact : "); Contact person = new
-	 * Contact(); person.getPersonDetails();
-	 * 
-	 * contacts.add(person); System.out.println("New Contact has been added"); }
-	 */
 	public void getContactDetails() {
 		System.out.println("Enter details : ");
 		Contact person = new Contact();
@@ -39,29 +32,6 @@ public class AddressBook extends Contact {
 			contacts.add(person);
 		}
 	}
-
-	/*
-	 * public void editPersonDetails() { Scanner sc = new Scanner(System.in);
-	 * System.out.println("Enter person name to edit : "); String name = sc.next();
-	 * 
-	 * for (Contact person : contacts) { if (person.getFirstName().equals(name)) {
-	 * System.out.println("Enter the option to edit : "); System.out.println(
-	 * "1.Firstname 2.Lastname  3.Address  4.City  5.State  6.Zipcode  7.Phone number  8.Email  9.Exit"
-	 * ); int choice = sc.nextInt(); switch (choice) { case 1:
-	 * System.out.println("Changing First name : "); person.setFirstName(sc.next());
-	 * break; case 2: System.out.println("Changing last name : ");
-	 * person.setLastName(sc.next()); break; case 3:
-	 * System.out.println("Changing address : "); person.setAddress(sc.next());
-	 * break; case 4: System.out.println("Changing city : ");
-	 * person.setCity(sc.next()); break; case 5:
-	 * System.out.println("Changing state : "); person.setState(sc.next()); break;
-	 * case 6: System.out.println("Changing zipcode : ");
-	 * person.setZipCode(sc.nextInt()); break; case 7:
-	 * System.out.println("Changing phone number: ");
-	 * person.setPhoneNumber(sc.nextInt()); break; case 8:
-	 * System.out.println("Changing email : "); person.setEmail(sc.next()); break;
-	 * case 9: System.out.println("Exiting from edit operation"); break; } } } }
-	 */
 
 	public void editPersonDetails() {
 		Scanner input1 = new Scanner(System.in);
@@ -114,14 +84,6 @@ public class AddressBook extends Contact {
 		}
 	}
 
-	/*
-	 * public void deleteContact() { Scanner sc = new Scanner(System.in);
-	 * System.out.println(contacts);
-	 * System.out.println("Enter Person Name to delete : "); String name =
-	 * sc.next(); for (Contact person : contacts) { if
-	 * (person.getFirstName().equals(name)) { contacts.remove(person);
-	 * System.out.println("Contact deleted successfully !!!"); break; } } }
-	 */
 	public void deleteContact() {
 		Scanner input1 = new Scanner(System.in);
 		System.out.println(contacts);
--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -7,33 +7,8 @@ public class AddressBookMain {
 
 	public static void main(String[] args) {
 
-		/*
-		 * String check = "Yes";
-		 * 
-		 * Contact person = new Contact();
-		 * 
-		 * // UC2-Adding a New Contact in AddressBook
-		 * 
-		 * book.getContactDetails(); addressBook.displayContact();
-		 * 
-		 * // UC3-Edit an Existing Contact in AddressBook using person name
-		 * 
-		 * addressBook.editContactDetails();
-		 * 
-		 * // UC4-Delete an Existing Contact in AddressBook using person name
-		 * 
-		 * addressBook.deleteContact();
-		 * 
-		 * // UC5- Add Multiple Person to AddressBook
-		 * 
-		 * 
-		 * while (check.equals("Yes")) { System.out.println("Adding a New Contact : ");
-		 * addressBook.addContactDetails(); addressBook.displayContact(); System.out.
-		 * println("If want to add more Contacts put 'Yes' else 'No' or AnyKey");
-		 * Scanner sc = new Scanner(System.in); check = sc.nextLine(); }
-		 */
-		  HashMap<String,AddressBook> dictionary = new HashMap<>();
-	        AddressBook book = new AddressBook();
+		HashMap<String, AddressBook> dictionary = new HashMap<>();
+		AddressBook book = new AddressBook();
 		Scanner input = new Scanner(System.in);
 		int option;
 		do {
